b56b49bfc7658806564c5ac09d6b05d77c5405ad
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-20 09:15:59 +0300

    new file:   Address Book/Learning/simple_calculator.py

---
+++ Address Book/Learning/simple_calculator.py
@@ -0,0 +1,39 @@
+def user_prompt():
+    x = float(input("What is x? "))
+    y = float(input("what is y? "))
+    return x, y
+
+def arithmetic_operations():
+    print("Please select an arithmetic operation:")
+    print("1: add (+)")
+    print("2: Sub (-)")
+    print("3: Mult (*)")
+    print("4: Div (/)")
+    
+    choice = input("Enter your choice (1/2/3/4) ")
+    valid_choices = ['1', '2','3', '4']
+    if choice not in valid_choices:
+        print("Invalid choice")
+        return
+    x, y = user_prompt()
+    
+    if choice == '1':
+        result = x + y
+        print("Result: ", result)
+    elif choice == '2':
+        result = x - y
+        print("Result: ", result)
+    elif choice == '3':
+        result = x * y
+        print("Result: ", result)
+    elif choice == '4':
+        try:
+            result = float(x / y)
+            print("Result: ", result)
+        except ZeroDivisionError:
+            print("Invalid division")
+        except TypeError:
+            pass
+    else:
+        print("Invalid choice,:\n[+, -, *,/]")
+arithmetic_operations()
