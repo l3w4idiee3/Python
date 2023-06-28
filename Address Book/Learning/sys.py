3c572a713d4ba47cbcf2858483769e5839684053
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-28 07:27:50 +0300

    added a comentabout sys.stdin

--- Address Book/Learning/sys.py
+++ Address Book/Learning/sys.py
@@ -3,7 +3,7 @@ def calculate_sum():
     total_sum = 0
     while True:
         try:
-            num = int(input("Enter a number to add or (non-integer to exit): "))
+            num = int(input("Enter a number to add or (non-integer to exit): "))#sys.stdin is used when you call the input() function
             total_sum += float(num)
         except ValueError:
             break
